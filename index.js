cfcedb7faa8925f1db3427b7dff2d07631e92dc5
Katherine Lewis <kale8432@colorado.edu>
2019-06-15 22:56:26 -0600

    Local storage

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var btn = $('.submit-btn')
 var container = $('.card-container')
 
+window.onload= function getLocalStorage() {
+    var resturant = JSON.parse(localStorage.getItem('resturant'))
+    var location = JSON.parse(localStorage.getItem('location'))
+   container.append(`
+    <p class="card">${resturant}, ${location}</p>`)
+}
 
 btn.on("click", addToDo)
 
@@ -10,6 +16,10 @@ function addToDo() {
   container.append(`
     <p class="card">${restInput}, ${locInput}</p>`)
   $('.input').val("")
+  localStorage.setItem('resturant', JSON.stringify(restInput))
+  localStorage.setItem('location', JSON.stringify(locInput))
+  var resturant = JSON.parse(localStorage.getItem('resturant'))
+  var location = JSON.parse(localStorage.getItem('location'))
 }
 
   
